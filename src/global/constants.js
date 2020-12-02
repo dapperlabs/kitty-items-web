e8f7455bf01388e684667b307bd39b55aaa4315b
James Hunter <github@qv.vg>
2020-12-02 13:05:37 -0800

    Init state and status synced in hook

---
+++ src/global/constants.js
@@ -0,0 +1,9 @@
+// Exposed states of a Flow Transaction
+export const IDLE = "IDLE"
+export const PROCESSING = "PROCESSING"
+export const SUCCESS = "SUCCESS"
+export const ERROR = "ERROR"
+
+// How long to pause on a success or error message
+// before transitioning back to an IDLE state.
+export const IDLE_DELAY = 2500
